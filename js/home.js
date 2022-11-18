9842fae62c862b96fa203d641f9b6c0b9e804693
UniqueElect <randomnayapo4ta@gmail.com>
2022-11-18 13:09:29 +0200

    first commit

---
+++ js/home.js
@@ -0,0 +1,26 @@
+function burgerFunction() {
+    var x = document.getElementById("navVisible");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+  
+let slides = document.getElementsByClassName("newz");
+let currentSlide = 0;
+document.getElementById("nav-button--next").addEventListener("click", () => {
+    changeSlide(currentSlide + 1)
+});
+document.getElementById("nav-button--prev").addEventListener("click", () => {
+    changeSlide(currentSlide - 1)
+});
+function changeSlide(moveTo) {
+    if (moveTo >= slides.length) {moveTo = 0;}
+    if (moveTo < 0) {moveTo = slides.length - 1;}
+    
+    slides[currentSlide].classList.toggle("active");
+    slides[moveTo].classList.toggle("active");
+
+    currentSlide = moveTo;
+}
